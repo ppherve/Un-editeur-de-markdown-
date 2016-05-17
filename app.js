be0f701f9c7f17d544a71ab685b7c7e40c4032ab
herve <perezherve31@gmail.com>
2016-05-17 10:03:45 +0200

    mise en forme recuperation des donner dans localstorage

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-setInterval(function() {
-	document.getElementById("code")
-	console.log('bip');
-},10000);
+
+var md = localStorage.getItem("code");
+var ta = document.getElementById("code");
+ta.value = md;
+var editor = CodeMirror.fromTextArea(ta, {
+    lineNumbers: true
+  });
+
+//setInterval(function() {
+//	var d = editor.getValue();
+//	console.log(d);
+//},1000);
+
+setInterval(function changeText(){
+	var d = editor.getValue();
+	document.getElementById("rendu").innerHTML = marked(d);
+	localStorage.setItem("code",d);
+},1000);
+
+
+
+
